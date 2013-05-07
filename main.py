0858a965b8126af7c336816c5847f766cbace9d9
Niket Desai <iamniket@gmail.com>
2013-05-07 01:45:29 -0700

    :metal: :customs:

--- main.py
+++ main.py
@@ -16,12 +16,16 @@
 #
 import webapp2
 import json
+import urllib
+import re
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         ip_address = self.request.remote_addr
+        addr = self.getAddressInfo(ip_address)
         glass = {
-          "address" : ip_address,
+          "address" : addr,
           "html": "<article>\n  <section>\n    <div class=\"layout-figure\">\n      <div class=\"align-center\">\n        <p class=\"text-x-large\">YOU</p>\n        <img src=\"http://pinkgirlq8.com/wp-content/uploads/taco-bell.png\" width=\"50\" height=\"60\">\n        <p class=\"text-x-large\">TB</p>\n      </div>\n      <div>\n        <div class=\"text-normal\">\n          <p>720 Story Road</p>\n          <p>San Jose, CA</p>\n          <p class=\"green\">Open</p>\n        </div>\n      </div>\n    </div>\n  </section>\n</article>\n",
           "notification": {
             "level": "DEFAULT"
@@ -29,7 +33,24 @@ class MainHandler(webapp2.RequestHandler):
         }        
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(glass))
-
+    
+    def getAddressInfo(self, ip_address):
+      ip_address = '107.212.24.150'
+      url = 'http://ipinfodb.com/ip_locator.php?ip=' + ip_address
+      url_file = urllib.urlopen(url)
+      contents = url_file.read()
+      
+      address_info = {}
+      address_info['city']   = re.search('<li>City : (.*)</li>', contents)
+      address_info['lat']    = re.search('<li>Latitude : (.*)</li>', contents)
+      address_info['long']   = re.search('<li>Longitude : (.*)</li>', contents)
+      address_info['zip']    = re.search('<li>Zip or postal code : (.*)</li>', contents)
+      
+      for item in address_info:
+        address_info[item] = address_info[item].group(1)
+      return json.dumps(address_info)
+  
+  
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
