d9f4dec702bc07ac06269654ca61151e117ddebb
Alex Manelis <amanelis@gmail.com>
2013-05-06 23:15:33 -0700

    :boom:

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('I want TB bitch')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
